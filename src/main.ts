fbb1f3dfeea2c8c3e169ccd74804954a883186c9
Alexandrea Defreitas <alex@motel.is>
2021-03-29 09:55:50 -0400

    chore: Integrate with radix class and rxjs to fetch wallet accounts (#8)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueRx from '@nopr3d/vue-next-rx'
 import './assets/tailwind.css'
 import '@/validations'
 
-createApp(App).use(router)
+createApp(App)
   .use(router)
+  .use(VueRx)
   .mount('#app')
