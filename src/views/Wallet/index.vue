66dec1b779fbc3125f4ac85171310b5f35da7d30
Caleb Oller <caleboller@gmail.com>
2021-04-27 21:14:28 -0400

    fix: clear balances on account create

--- src/views/Wallet/index.vue
+++ src/views/Wallet/index.vue
@@ -213,6 +213,7 @@ const WalletIndex = defineComponent({
       getDerivedAccountsIndex()
         .then((index: string) => {
           saveDerivedAccountsIndex(Number(index) + 1)
+          tokenBalances.value = []
           wallet.deriveNextAccount({ alsoSwitchTo: true })
         })
     }
