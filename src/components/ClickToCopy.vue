ea56d071b01c7f2d2f6bf5cee2be977dd7672419
Caleb Oller <caleboller@gmail.com>
2021-04-28 11:30:52 -0400

    feat: improve copy icon clickability

--- src/components/ClickToCopy.vue
+++ src/components/ClickToCopy.vue
@@ -1,10 +1,10 @@
 <template>
-  <div @click.stop="copyText" class="pointer-events-auto cursor-pointer flex items-center">
+  <button @click.stop="copyText" class="cursor-pointer flex items-center active:text-rBlack outline-none focus:outline-none">
     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect x="7.5" y="7.5" width="7" height="7" class="stroke-current"/>
       <path d="M13 5H5V13" class="stroke-current"/>
     </svg>
-  </div>
+  </button>
 </template>
 
 <script lang="ts">
