e259f7d1ba0e864b9953f636491cdae5f3b5845e
Caleb Oller <caleboller@gmail.com>
2021-04-28 10:09:48 -0400

    feat: add some animations to submit button

--- src/components/ButtonSubmit.vue
+++ src/components/ButtonSubmit.vue
@@ -2,7 +2,7 @@
   <button
     type="submit"
     class="inline-flex items-center justify-center px-6 py-4 border font-normal leading-snug rounded transition-colors w-72"
-    :class="{ 'bg-rGray border-rGray text-rGrayDark cursor-not-allowed': disabled, 'bg-rGreen border-rGreen text-white': !disabled }"
+    :class="{ 'bg-rGray border-rGray text-rGrayDark cursor-not-allowed': disabled, 'bg-rGreen border-4 border-rGreen text-white hover:bg-rGreenDark active:bg-rGray': !disabled }"
     :disabled="disabled"
   >
     <slot></slot>
