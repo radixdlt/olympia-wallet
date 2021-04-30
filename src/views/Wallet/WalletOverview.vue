2cdfad0d63e3e277f66fc1eab0af8e08158a2fa1
Caleb Oller <caleboller@gmail.com>
2021-04-30 09:47:32 -0400

    chore: update free token button title

--- src/views/Wallet/WalletOverview.vue
+++ src/views/Wallet/WalletOverview.vue
@@ -41,7 +41,7 @@
     <div class="bg-white text-rBlack py-7 px-8 flex-1">
       <div class="font-medium mb-8 flex items-center justify-between">
         {{ $t('wallet.additionalBalancesHeading') }}
-        <button @click="$emit('requestFreeTokens')" class="focus:outline-none text-rGreen py-2 px-4 border border-rGreen rounded-md hover:bg-translucent-greenLighter active:bg-translucent-greenLight">Get free tokens</button>
+        <button @click="$emit('requestFreeTokens')" class="focus:outline-none text-rGreen py-2 px-4 border border-rGreen rounded-md hover:bg-translucent-greenLighter active:bg-translucent-greenLight">Get free betanet tokens</button>
       </div>
 
       <div class="flex flex-row flex-wrap justify-between -mx-9">
@@ -102,7 +102,6 @@ const WalletOverview = defineComponent({
     totalXRD (): AmountT {
       if (!this.nativeToken) return Amount.fromUnsafe(0)._unsafeUnwrap()
       if (!this.tokenBalances.tokenBalances) return Amount.fromUnsafe(0)._unsafeUnwrap()
-      console.log(this.tokenBalances)
       const xrdTokenBalance = this.tokenBalances.tokenBalances.find((tb: TokenBalance) => tb.token.rri.equals(this.nativeToken!.rri))
       if (!xrdTokenBalance) return Amount.fromUnsafe(0)._unsafeUnwrap()
       const xrdAmount = Amount.fromUnsafe(xrdTokenBalance.amount)
