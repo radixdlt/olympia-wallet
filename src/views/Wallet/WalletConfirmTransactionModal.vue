0a2f1933c5feda97f7b92c92976854f0ea13ba1b
Caleb Oller <caleb@motel.is>
2021-06-03 21:17:00 -0400

    chore: minor staking cleanup (#97)

--- src/views/Wallet/WalletConfirmTransactionModal.vue
+++ src/views/Wallet/WalletConfirmTransactionModal.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fixed w-screen h-screen z-20 flex items-center justify-center bg-translucent-black">
-    <div class="h-modal bg-white rounded-md py-7 px-7 w-full max-w-3xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <div class="h-modal bg-white rounded-md py-7 px-7 w-full max-w-3xl absolute top-1/2 left-1/2 transform -translate-x-1/3 -translate-y-1/2">
       <div v-if="transactionState === 'building'">
         <div class="bg-white h-full flex flex-col flex-1 w-full justify-around box-border mt-52">
           <svg width="40" height="40" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg" class="container animate-spin">
@@ -11,6 +11,12 @@
           <div class="text-center mt-8 text-rGrayDark text-lg">
             Building Transaction...
           </div>
+          <button
+            class="text-rGrayDark py-4 px-4 text-sm mx-auto mt-4"
+            @click="canCancel && $emit('cancel')"
+          >
+            {{ $t('transaction.cancelButton') }}
+          </button>
         </div>
       </div>
       <div v-if="transactionState === 'submitting'">
