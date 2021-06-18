0bae8c057facef9f64ad6b96e6cb42412368c8f6
AbstractFruitFactory <alexander.wormbs@gmail.com>
2021-06-18 19:13:43 +0100

    wip

--- src/views/Wallet/index.vue
+++ src/views/Wallet/index.vue
@@ -624,7 +624,6 @@ const WalletIndex = defineComponent({
         })
       }).subscribe((hwAccount: any) => {
         console.log('got hw account: ', hwAccount.address.toString())
-        
       })
     }
 
