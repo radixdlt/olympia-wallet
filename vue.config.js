b765dccb2985fa70ae8f8513c4d4cb811075efca
Alexander Wormbs <alexander.wormbs@gmail.com>
2021-07-01 11:49:40 +0100

    copy over native modules to bundle

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,34 @@
-const TerserPlugin = require('terser-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const NormalModuleReplacementPlugin = require('webpack').NormalModuleReplacementPlugin
 
 module.exports = {
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
+
       chainWebpackRendererProcess: config => {
         config.target('web')
       },
-     
+      chainWebpackMainProcess: config => {
+        config.plugin('CopyWebpackPlugin').use(CopyWebpackPlugin, [
+          {
+            patterns: [
+              'node_modules/@aleworm/usb/build/Release/usb_bindings.node',
+              'node_modules/@aleworm/node-hid/build/Release/HID_hidraw.node'
+            ]
+          }
+        ])
+
+        config
+          .plugin('NormalModuleReplacementPlugin')
+          .use(NormalModuleReplacementPlugin, [
+            /^bindings$/,
+            `${__dirname}/src/bindings`
+          ])
+      },
+
       builderOptions: {
+        asar: false,
         publish: {
           provider: 'github',
           private: true,
