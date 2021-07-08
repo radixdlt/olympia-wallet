bfa07697fddc23ad1b464891208ba90ddfc11cc0
Scott Taylor <staylorwr@users.noreply.github.com>
2021-07-08 12:31:19 -0400

    dist_electron not dist

--- move_dist.sh
+++ move_dist.sh
@@ -2,8 +2,8 @@
 
 mkdir dist-linux dist-mac dist-win > /dev/null 2>&1
 
-mv dist/latest-linux.yml dist/*.AppImage dist/*.tar.gz dist/*.snap dist-linux > /dev/null 2>&1
-mv dist/latest-mac.yml dist/*.dmg dist/*.dmg.blockmap dist-mac > /dev/null 2>&1
-mv dist/latest.yml dist/*.exe dist/*.exe.blockmap dist-win > /dev/null 2>&1
+mv dist_electron/latest-linux.yml dist_electron/*.AppImage dist_electron/*.tar.gz dist_electron/*.snap dist-linux > /dev/null 2>&1
+mv dist_electron/latest-mac.yml dist_electron/*.dmg dist_electron/*.dmg.blockmap dist-mac > /dev/null 2>&1
+mv dist_electron/latest.yml dist_electron/*.exe dist_electron/*.exe.blockmap dist-win > /dev/null 2>&1
 
 exit 0
