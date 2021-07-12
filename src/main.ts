eeac7098265d0a26c921e366b277dadc053975be
Scott Taylor <staylorwr@users.noreply.github.com>
2021-07-12 11:01:28 -0400

    chore: Add vue-toastification

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueRx from '@nopr3d/vue-next-rx'
+import Toast, { PluginOptions, POSITION } from 'vue-toastification'
 import './assets/tailwind.css'
 import '@/validations'
 import { store, key } from '@/store'
@@ -9,9 +10,14 @@ import { i18n } from '@/text'
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
+const options: PluginOptions = {
+  position: POSITION.BOTTOM_RIGHT
+}
+
 createApp(App)
   .use(router)
   .use(VueRx)
   .use(store, key)
   .use(i18n)
+  .use(Toast, options)
   .mount('#app')
