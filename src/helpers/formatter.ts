c443eedccd120b32f30d59faf806580a6b7edd90
Caleb Oller <caleboller@gmail.com>
2021-07-12 10:35:15 -0400

    chore: shorten address display

--- src/helpers/formatter.ts
+++ src/helpers/formatter.ts
@@ -2,5 +2,5 @@ import { AccountAddressT } from '@radixdlt/application'
 
 export const formatAddressForDisplay = function (address: AccountAddressT) {
   const s = address.toString()
-  return s.substring(0, 8) + '...' + s.substring(s.length - 8)
+  return s.substring(0, 6) + '...' + s.substring(s.length - 6)
 }
