2c4a6db15e451404650cd744eb2aa9b866bebedd
Scott Taylor <staylorwr@users.noreply.github.com>
2021-07-13 15:07:48 -0400

    feat: copying addresses in wallet and explorer (#165)
    * chore: Add vue-toastification
    
    * s
    
    * chore(deps): Upgrade Vue, add default styles for vue-toastification

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,27 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueRx from '@nopr3d/vue-next-rx'
+import Toast, { PluginOptions, POSITION } from 'vue-toastification'
 import './assets/tailwind.css'
+import 'vue-toastification/dist/index.css'
 import '@/validations'
 import { store, key } from '@/store'
 import { i18n } from '@/text'
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
+const options: PluginOptions = {
+  position: POSITION.BOTTOM_RIGHT,
+  closeButton: false,
+  icon: false,
+  maxToasts: 3,
+  hideProgressBar: true
+}
+
 createApp(App)
   .use(router)
   .use(VueRx)
   .use(store, key)
   .use(i18n)
+  .use(Toast, options)
   .mount('#app')
