cd165dffbc7ad3a0561c038da2ec2e2929b77be9
Scott Taylor <staylorwr@users.noreply.github.com>
2021-07-15 16:02:42 -0400

    fix: Validator, Account, and Transaction IDs should have specific shortened formats

--- src/components/ActionListItemUnstakeTokens.vue
+++ src/components/ActionListItemUnstakeTokens.vue
@@ -9,7 +9,7 @@
     </div>
     <div class="flex flex-col items-end">
       <div class="flex flex-row flex-1 min-w-0 items-center">
-        <span>{{ $t('history.validatorLabel') }}:</span> <span class="ml-2 mr-1 min-w-0 font-mono">{{ displayAddress(action.validator) }}</span>
+        <span>{{ $t('history.validatorLabel') }}:</span> <span class="ml-2 mr-1 min-w-0 font-mono">{{ displayAddress }}</span>
         <click-to-copy :text="action.validator.toString()" />
       </div>
     </div>
@@ -18,9 +18,9 @@
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
-import { ExecutedUnstakeTokensAction, AccountAddressT, Token } from '@radixdlt/application'
+import { ExecutedUnstakeTokensAction, Token } from '@radixdlt/application'
 import ClickToCopy from '@/components/ClickToCopy.vue'
-import { formatAddressForDisplay } from '@/helpers/formatter'
+import { formatValidatorAddressForDisplay } from '@/helpers/formatter'
 import BigAmount from '@/components/BigAmount.vue'
 
 const ActionListItemUnstakeTokens = defineComponent({
@@ -43,10 +43,9 @@ const ActionListItemUnstakeTokens = defineComponent({
       required: true
     }
   },
-
-  methods: {
-    displayAddress (address: AccountAddressT): string {
-      return formatAddressForDisplay(address)
+  computed: {
+    displayAddress (): string {
+      return formatValidatorAddressForDisplay(this.action.validator)
     }
   }
 })
