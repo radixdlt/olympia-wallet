14f130b85fd783af3470d8d41983f60cf672b159
Scott Taylor <staylorwr@users.noreply.github.com>
2021-07-15 16:11:05 -0400

    fix: Add more menu items so we can copy and paste in the app (#178)

--- src/menu.ts
+++ src/menu.ts
@@ -19,6 +19,28 @@ const template = [
       { type: 'separator' },
       { role: 'quit' }
     ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+      { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+      { type: 'separator' },
+      { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+      { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+      { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+      { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      { role: 'close' },
+      { role: 'minimize' },
+      { role: 'zoom' },
+      { type: 'separator' },
+      { role: 'front' }
+    ]
   }
 ]
 
