b8c25533f85b4cc8b0b9422a0283c7f6cc6fa8df
AbstractFruitFactory <alexander.wormbs@gmail.com>
2021-07-19 19:38:35 +0100

    Alexw ledger connection (#187)
    * wip
    
    * deps
    
    Co-authored-by: Caleb Oller <caleb@motel.is>

--- src/actions/electron/create-wallet.ts
+++ src/actions/electron/create-wallet.ts
@@ -2,8 +2,6 @@ import { IpcMainEvent, IpcMainInvokeEvent } from 'electron/main'
 import { clipboard } from 'electron'
 import crypto from 'crypto'
 import { store } from '@/actions/electron/data-store'
-import { BasicLedgerTransport } from '@radixdlt/hardware-ledger'
-import TransportNodeHid from '@ledgerhq/hw-transport-node-hid'
 
 export const writeKeystoreFile = (event: IpcMainInvokeEvent, encodedWallet: string) => store.set('seed', encodedWallet)
 
@@ -24,11 +22,3 @@ export const copyToClipboard = (event: IpcMainEvent, text: string) => {
 
 export const validatePin = (event: IpcMainInvokeEvent, pin: string) =>
   digestPin(pin).then((inputHash: string) => store.get('pin') === inputHash)
-
-const basicLedgerTransportPromise: Promise<BasicLedgerTransport> = TransportNodeHid.create()
-
-export const sendAPDU = async (event: IpcMainInvokeEvent, apdu: { cla: number, ins: number, p1: number, p2: number, data?: string }) => {
-  const transport = await basicLedgerTransportPromise
-  const result = await transport.send(apdu.cla, apdu.ins, apdu.p1, apdu.p2, apdu.data ? Buffer.from(apdu.data, 'hex') : undefined)
-  return result
-}
