e08ded60e2d33c92ba7b4c7eb3da73304186e8db
AbstractFruitFactory <alexander.wormbs@gmail.com>
2021-07-20 13:54:50 +0100

    close and reopen connection on call

--- src/actions/electron/hardware-wallet.ts
+++ src/actions/electron/hardware-wallet.ts
@@ -1,15 +1,9 @@
-import {  Device } from '@radixdlt/hardware-ledger'
 import TransportNodeHid from '@ledgerhq/hw-transport-node-hid'
 import { IpcMainInvokeEvent } from 'electron/main'
 
 export const sendAPDU = async (event: IpcMainInvokeEvent, apdu: { cla: number, ins: number, p1: number, p2: number, data?: string }) => {
-  const device = await openConnection()
-  return await device.send(apdu.cla, apdu.ins, apdu.p1, apdu.p2, apdu.data ? Buffer.from(apdu.data, 'hex') : undefined)
-}
-
-const openConnection = async (): Promise<Device> => {
-  const devices = await TransportNodeHid.list()
-
-  if (!devices[0]) { throw new Error('No device found.') }
-  return TransportNodeHid.open(devices[0]) as any
+  const transport = await TransportNodeHid.create()
+  const result = await transport.send(apdu.cla, apdu.ins, apdu.p1, apdu.p2, apdu.data ? Buffer.from(apdu.data, 'hex') : undefined)
+  transport.close()
+  return result
 }
