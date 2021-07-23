e2aade355f89a70ef9478948132f0c5bf4d0c00b
Caleb Oller <caleboller@gmail.com>
2021-07-23 15:58:00 -0400

    chore: update icons

--- src/views/Wallet/WalletSidebarDefault.vue
+++ src/views/Wallet/WalletSidebarDefault.vue
@@ -24,14 +24,14 @@
         @click="$emit('setView', 'overview')"
       >
        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-3">
-        <path d="M6.47401 13.5557L6.73695 14.4763C6.73703 16.0568 5.45579 17.3379 3.87531 17.3379C2.29491 17.3379 1.01367 16.0567 1.01367 14.4763L1.24568 13.6037" stroke="#00C389" stroke-width="1.5" stroke-miterlimit="10"/>
-        <path d="M22.9721 13.541L23.235 14.4616C23.2351 16.0421 21.9538 17.3233 20.3734 17.3233C18.7929 17.3233 17.5117 16.042 17.5117 14.4616L17.7437 13.5891" stroke="#00C389" stroke-width="1.5" stroke-miterlimit="10"/>
-        <path d="M12.1064 22.9783V19.6475" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
-        <path d="M3.94141 23.5H20.2708" stroke="#00C389" stroke-width="1.5" stroke-miterlimit="10"/>
-        <path d="M12.1064 16.7533V0" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
+        <path d="M6.47435 13.5559L6.73728 14.4765C6.73736 16.057 5.45613 17.3382 3.87565 17.3382C2.29524 17.3382 1.01401 16.0569 1.01401 14.4765L1.24602 13.604" stroke="#00C389" stroke-width="1.5" stroke-miterlimit="10"/>
+        <path d="M22.9724 13.5411L23.2353 14.4618C23.2354 16.0423 21.9541 17.3234 20.3737 17.3234C18.7932 17.3234 17.512 16.0422 17.512 14.4618L17.744 13.5892" stroke="#00C389" stroke-width="1.5" stroke-miterlimit="10"/>
+        <path d="M12.1068 22.9787V19.6478" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
+        <path d="M3.94185 23.5H20.2713" stroke="#00C389" stroke-width="1.5" stroke-miterlimit="10"/>
+        <path d="M12.1068 16.7533V0" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
         <path d="M23.1966 14.3256L20.2714 4.08594H3.94202L1 14.4617" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
-        <path d="M20.2711 4.08594L17.5117 14.4617" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
-        <path d="M6.72266 14.4617L3.94141 4.08594" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
+        <path d="M20.2714 4.08594L17.512 14.4617" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
+        <path d="M6.7231 14.4617L3.94185 4.08594" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
       </svg>
 
         <div class="font-normal text-white">{{ $t('wallet.navBalances') }}</div>
@@ -42,14 +42,12 @@
         :class="{ 'opacity-70': activeView !== 'transaction' }"
         @click="$emit('setView', 'transaction')"
       >
-        <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-3">
-          <path d="M0 10.7441H15.5762" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
-          <path d="M5.94873 5.75879H21.5982" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
-          <path d="M5.93506 15.7588H21.579" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
-          <path d="M15.8271 0.75H21.5982" stroke="#00C389" stroke-width="1.5" stroke-miterlimit="10"/>
-          <path d="M15.8271 20.75H21.5982" stroke="#00C389" stroke-width="1.5" stroke-miterlimit="10"/>
-          <path d="M18.0728 10.7441H21.5985" stroke="#00C389" stroke-width="1.5" stroke-miterlimit="10"/>
+        <svg width="28" height="20" viewBox="0 0 28 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-3">
+          <path d="M27 9.8871L14.4194 1.5V9.8871" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M9.3871 9.88707H27L14.3338 18.2742V13.2419" stroke="#00C389" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M9.3871 9.88715C9.3871 8.94326 9.06866 8.02698 8.48331 7.28651C7.89795 6.54605 7.07991 6.02472 6.16151 5.80685C5.24311 5.58898 4.27806 5.68731 3.42248 6.08594C2.5669 6.48458 1.87083 7.1602 1.44685 8.00351C1.02288 8.84682 0.895806 9.8085 1.08619 10.733C1.27657 11.6575 1.77327 12.4907 2.49595 13.0979C3.21863 13.7051 4.12501 14.0507 5.06848 14.0788C6.01195 14.107 6.93733 13.816 7.69492 13.253" stroke="#00C389" stroke-width="1.5" stroke-linecap="round"/>
         </svg>
+
         <div class="font-normal text-white">{{ $t('wallet.navTransaction') }}</div>
       </div>
 
@@ -58,10 +56,11 @@
         :class="{ 'opacity-70': activeView !== 'staking' }"
         @click="$emit('setView', 'staking')"
       >
-        <svg width="20" height="25" viewBox="0 0 20 25" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-3">
-          <path d="M3.85449 10.1179V7.65369C3.85449 4.29719 6.57551 1.57617 9.93201 1.57617C13.2885 1.57617 16.0095 4.29719 16.0095 7.65369V10.1179" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
-          <path d="M1.57617 24.1513V9.92188H4.0387H15.875H18.3376V23.4034H5.06131" stroke="#00C389" stroke-width="1.5" stroke-miterlimit="10"/>
+        <svg width="19" height="24" viewBox="0 0 19 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-3">
+          <path d="M3.27832 9.8159V7.35169C3.27832 3.99519 5.99934 1.27417 9.35584 1.27417C12.7123 1.27417 15.4334 3.99519 15.4334 7.35169V9.8159" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
+          <path d="M1 23.8483V9.6189H3.46252H15.2989H17.7614V23.1004H4.48513" stroke="#00C389" stroke-width="1.5" stroke-miterlimit="10"/>
         </svg>
+
         <div class="font-normal text-white">{{ $t('wallet.navStake') }}</div>
       </div>
 
@@ -70,12 +69,11 @@
         :class="{ 'opacity-70': activeView !== 'history' }"
         @click="$emit('setView', 'history')"
       >
-        <svg width="23" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-3">
-          <path d="M1 1H9.46735V8.93388H1.53347V4.26133" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
-          <path d="M21.5291 1H13.5952V8.93388H21.5291V1Z" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
-          <path d="M9.46757 13.0664H1.53369V21.0003H9.46757V13.0664Z" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
-          <path d="M21.5291 13.0664H13.5952V21.0003H21.5291V13.0664Z" stroke="#00C389" stroke-width="1.5" stroke-miterlimit="10"/>
+        <svg width="20" height="24" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-3">
+          <path d="M1 13.3484V9.84839L19 9.84839V16.3484H1V22.8484H19V18.8484" stroke="white" stroke-width="1.5"/>
+          <path d="M1 3.34839V6.84839H19V0.848389H0" stroke="#00C389" stroke-width="1.5"/>
         </svg>
+
         <div class="font-normal text-white">{{ $t('wallet.navHistory') }}</div>
       </div>
     </div>
@@ -86,9 +84,9 @@
         target="__blank"
         class="flex flex-row items-center my-2 cursor-pointer transition-opacity"
       >
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-3">
-          <path d="M11.528 14.224V11.552C12.4667 11.52 13.192 11.2907 13.704 10.864C14.2267 10.4373 14.488 9.86667 14.488 9.152V8.848C14.488 8.24 14.296 7.75467 13.912 7.392C13.528 7.01867 13 6.832 12.328 6.832C11.6347 6.832 11.0853 7.01333 10.68 7.376C10.2853 7.73867 10.008 8.224 9.848 8.832L9 8.528C9.10667 8.16533 9.25067 7.82933 9.432 7.52C9.624 7.21067 9.85333 6.944 10.12 6.72C10.3973 6.496 10.7227 6.32 11.096 6.192C11.4693 6.064 11.8907 6 12.36 6C13.3307 6 14.0933 6.26667 14.648 6.8C15.2027 7.33333 15.48 8.05333 15.48 8.96C15.48 9.45067 15.4 9.888 15.24 10.272C15.08 10.6453 14.856 10.9707 14.568 11.248C14.2907 11.5147 13.9653 11.728 13.592 11.888C13.2293 12.048 12.84 12.16 12.424 12.224V14.224H11.528ZM11.976 17.504C11.72 17.504 11.528 17.44 11.4 17.312C11.2827 17.184 11.224 17.008 11.224 16.784V16.608C11.224 16.384 11.2827 16.208 11.4 16.08C11.528 15.952 11.72 15.888 11.976 15.888C12.2427 15.888 12.4347 15.952 12.552 16.08C12.68 16.208 12.744 16.384 12.744 16.608V16.784C12.744 17.008 12.68 17.184 12.552 17.312C12.4347 17.44 12.2427 17.504 11.976 17.504Z" fill="#DDE5ED"/>
-          <circle cx="12" cy="12" r="11.5" stroke="#DDE5ED"/>
+        <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-3">
+          <path d="M11.496 14.8804V12.2564C12.3707 12.2137 13.0533 12.0004 13.544 11.6164C14.0453 11.2324 14.296 10.7044 14.296 10.0324V9.80839C14.296 9.26439 14.1253 8.83239 13.784 8.51239C13.4427 8.18172 12.984 8.01639 12.408 8.01639C11.7893 8.01639 11.2933 8.19239 10.92 8.54439C10.5573 8.89639 10.312 9.34439 10.184 9.88839L9 9.44039C9.096 9.09906 9.23467 8.77372 9.416 8.46439C9.608 8.14439 9.84267 7.86706 10.12 7.63239C10.408 7.38706 10.744 7.19506 11.128 7.05639C11.512 6.91772 11.9547 6.84839 12.456 6.84839C13.448 6.84839 14.232 7.12039 14.808 7.66439C15.384 8.20839 15.672 8.94439 15.672 9.87239C15.672 10.3844 15.5813 10.8324 15.4 11.2164C15.2293 11.5897 15.0053 11.9097 14.728 12.1764C14.4507 12.4431 14.136 12.6564 13.784 12.8164C13.432 12.9764 13.08 13.0884 12.728 13.1524V14.8804H11.496ZM12.12 18.4004C11.7787 18.4004 11.528 18.3151 11.368 18.1444C11.2187 17.9737 11.144 17.7497 11.144 17.4724V17.2644C11.144 16.9871 11.2187 16.7631 11.368 16.5924C11.528 16.4217 11.7787 16.3364 12.12 16.3364C12.4613 16.3364 12.7067 16.4217 12.856 16.5924C13.016 16.7631 13.096 16.9871 13.096 17.2644V17.4724C13.096 17.7497 13.016 17.9737 12.856 18.1444C12.7067 18.3151 12.4613 18.4004 12.12 18.4004Z" fill="white"/>
+          <circle cx="12.5" cy="12.3484" r="11.5" stroke="#00C389" stroke-width="1.5"/>
         </svg>
 
         <div class="font-normal text-white">{{ $t('wallet.navHelp') }}</div>
@@ -101,7 +99,8 @@
         @click="$emit('setView', 'settings')"
       >
         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-3">
-          <path d="M21.0003 13.0001V8.99997H18.2223C18.0571 8.40186 17.8224 7.83417 17.5227 7.30598L19.4856 5.34296L16.6571 2.51449L14.6942 4.47752C14.1661 4.17779 13.5981 3.94319 13.0002 3.77781V1H9.00011V3.77781C8.40229 3.94312 7.83424 4.17772 7.30619 4.47752L5.34331 2.51442L2.51485 5.34289L4.47773 7.30591C4.17793 7.83367 3.9434 8.40179 3.77802 8.9999H1V13H3.77802C3.9434 13.5981 4.17793 14.1658 4.47766 14.694L2.51485 16.6567L5.34331 19.4852L7.30612 17.5225C7.83417 17.8222 8.40222 18.0568 9.00018 18.2222V21H13.0002V18.2222C13.5982 18.0568 14.1663 17.8222 14.6943 17.5225L16.6572 19.4852L19.4856 16.6567L17.5228 14.694C17.8225 14.1659 18.0571 13.5981 18.2223 13H21.0003V13.0001ZM11.0002 15C8.79105 15 7.00012 13.2092 7.00012 10.9998C7.00012 8.79077 8.79105 7.00005 11.0002 7.00005C13.2094 7.00005 15.0002 8.79084 15.0002 10.9998C15.0002 13.2092 13.2094 15 11.0002 15Z" stroke="white" stroke-miterlimit="10"/>
+          <path d="M21.0003 8.84836V12.8485L19.5 12.8484L18.2223 12.8484C18.0571 13.4465 17.8225 14.0143 17.5228 14.5424L19.4856 16.5051L16.6572 19.3335L14.6943 17.3709C14.1663 17.6706 13.5982 17.9052 13.0002 18.0706V20.8484H9.00018V18.0706C8.40222 17.9052 7.83417 17.6706 7.30612 17.3709L5.34331 19.3335L2.51485 16.5051L4.47766 14.5424C4.17793 14.0142 3.9434 13.4465 3.77802 12.8484H1V8.84829H3.77802C3.9434 8.25018 4.17793 7.68206 4.47773 7.1543L2.51485 5.19128L5.34331 2.36281L7.30619 4.3259C7.83424 4.02611 8.40229 3.79151 9.00011 3.6262V0.848389H13.0002V3.6262C13.5981 3.79158 14.1661 4.02618 14.6942 4.3259L16.6571 2.36288L19.4856 5.19135L17.5227 7.15437C17.8224 7.68256 18.0571 8.25025 18.2223 8.84836H21.0003Z" stroke="white" stroke-width="1.5" stroke-miterlimit="10"/>
+          <circle cx="11" cy="10.8484" r="3.75" stroke="white" stroke-width="1.5"/>
         </svg>
 
         <div class="font-normal text-white">{{ $t('wallet.navSettings') }}</div>
