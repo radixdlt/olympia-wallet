273c1b96657f5afd862d1a9c65de73594bc83043
Scott Taylor <staylorwr@users.noreply.github.com>
2021-07-23 14:28:43 -0400

    fix: Stake List was using an unpredicatable key (#215)
    The list of current positions displayed on the stake page was depending
    on the index of the stake in the sorted list as a key.  Over time, the
    index of a position can change on the sorted list as a user interacts
    with the positions and as the values of the positions change from
    polling.
    
    This commit fixes these inconsistencies by depending on the address of
    the valdiator as a key, extending the `Position` type by adding the
    address as a string.
    
    This PR also fixes an out of date `CopyToClipboard` call that was
    sending a `text` prop rather then the updated `address` prop.

--- src/store/_types.ts
+++ src/store/_types.ts
@@ -1,6 +1,7 @@
 import { ValidatorAddressT, StakePosition, UnstakePosition } from '@radixdlt/application'
 
 export declare type Position = Readonly<{
+  address: string;
   validator: ValidatorAddressT;
   stakes: Array<StakePosition>;
   unstakes: Array<UnstakePosition>;
