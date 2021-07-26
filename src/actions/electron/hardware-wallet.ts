b77fa46f50d9eb45582980acaf310a8bedecd328
AbstractFruitFactory <alexander.wormbs@gmail.com>
2021-07-26 14:49:03 +0100

    change transport method

--- src/actions/electron/hardware-wallet.ts
+++ src/actions/electron/hardware-wallet.ts
@@ -2,8 +2,12 @@ import TransportNodeHid from '@ledgerhq/hw-transport-node-hid'
 import { IpcMainInvokeEvent } from 'electron/main'
 
 export const sendAPDU = async (event: IpcMainInvokeEvent, apdu: { cla: number, ins: number, p1: number, p2: number, data?: string }) => {
-  const transport = await TransportNodeHid.create()
-  const result = await transport.send(apdu.cla, apdu.ins, apdu.p1, apdu.p2, apdu.data ? Buffer.from(apdu.data, 'hex') : undefined)
-  transport.close()
+  const paths = await TransportNodeHid.list()
+  if (paths.length === 0) throw Error('No device found.')
+
+  const device = await TransportNodeHid.open(paths[0])
+
+  const result = await device.send(apdu.cla, apdu.ins, apdu.p1, apdu.p2, apdu.data ? Buffer.from(apdu.data, 'hex') : undefined)
+  await device.close()
   return result
 }
