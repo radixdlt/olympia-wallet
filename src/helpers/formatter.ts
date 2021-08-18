028d9fe56b1f3c48ba7d9104f834a7a1b24af255
Samuel Yeo <yeosamuel95@gmail.com>
2021-08-18 00:13:15 -0400

    chore: abstract function and create multiple helpers

--- src/helpers/formatter.ts
+++ src/helpers/formatter.ts
@@ -1,4 +1,4 @@
-import { AccountAddressT, HRP, Network, ValidatorAddressT, TokenBalance } from '@radixdlt/application'
+import { AccountAddressT, HRP, ValidatorAddressT } from '@radixdlt/application'
 
 export const formatWalletAddressForDisplay = function (address: AccountAddressT): string {
   const s = address.toString()
@@ -10,26 +10,44 @@ export const formatValidatorAddressForDisplay = function (address: ValidatorAddr
   return s.substring(0, 2) + '...' + s.substring(s.length - 9)
 }
 
-export const truncateRRIStringForDisplay = function (rriString: string): any {
-  const arrOfValues = []
-  for (const [key, val] of Object.entries(HRP)) {
-    arrOfValues.push(val.RRI_suffix)
-  }
-
-  // Sort by largest -> Smallest since some suffixes have length 3 and some have 2
+export const truncateRRIStringForDisplay = function (rriString: string): string {
+  // Sort by largest -> Smallest since some suffixes have length 4 and some have 3
   // Avoiding scenarios such as matching '_tr' first when there is a '_tr3'
-  arrOfValues.sort(function (a, b) {
+  // If no RRI suffix match, will return regular string
+  // Return original RRI String if <= 21, following Wireframe design examples
+  if (rriString.length <= 21) {
+    return rriString
+  }
+  const sortedHRPKeys = getSortedHRPKeys()
+  const correctRRISuffix = getCorrectRRISuffixFromHRP(sortedHRPKeys, rriString)
+  // Catch in case no RRI Suffix was found in Networks/HRP
+  if (correctRRISuffix === '') {
+    return rriString
+  }
+  const prefixLength = getRRIPrefixLength(correctRRISuffix, rriString)
+  return rriString.substring(0, prefixLength) + '...' + rriString.substring(rriString.length - 9)
+}
+
+const getSortedHRPKeys = function (): string[] {
+  const allHRPKeys = []
+  for (const [key, val] of Object.entries(HRP)) {
+    allHRPKeys.push(val.RRI_suffix)
+  }
+  allHRPKeys.sort(function (a, b) {
     return b.length - a.length
   })
+  return allHRPKeys
+}
 
-  const correctSuffix = arrOfValues.filter((suffix) => {
+const getCorrectRRISuffixFromHRP = function (hrpKeys: string[], rriString: string): string {
+  // Using regex /${suffix}/.exec(rriString) to return the right suffix
+  const rriStringMatchHRPKey = hrpKeys.filter((suffix) => {
     return rriString.match(suffix)! != null
   })
+  return rriStringMatchHRPKey[0]
+}
 
-  const charsUntilSuffixEnd = correctSuffix.map((suffix) => {
-    return rriString.match(suffix)!.index // suffix.length // + matchDetails[1]
-  })
-
-  const prefixLength = charsUntilSuffixEnd[0]! + correctSuffix[0].length
-  return rriString.substring(0, prefixLength) + '...' + rriString.substring(rriString.length - 9)
+const getRRIPrefixLength = function (rriSuffix: string, rriString: string): number {
+  const match = rriString.match(rriSuffix)!
+  return match.index! + rriSuffix.length
 }
