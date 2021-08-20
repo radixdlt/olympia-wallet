332273efbc3f3d91db6e74848b890840169e60f6
Sam Y <SK-Sam@users.noreply.github.com>
2021-08-20 00:30:02 -0700

    feat: show non xrd tokens in wallet
    * chore: design other token balance divs
    
    * chore: add margin righ to arrow url
    
    * feat: add RRI truncating feature to oadditional balances
    
    * fix: hmake RRI truncation dynamic based on correct suffix
    
    * chore: add hover effect to click-to-copy element
    
    * chore: abstract function and create multiple helpers

--- src/helpers/formatter.ts
+++ src/helpers/formatter.ts
@@ -1,4 +1,4 @@
-import { AccountAddressT, ValidatorAddressT } from '@radixdlt/application'
+import { AccountAddressT, HRP, ValidatorAddressT } from '@radixdlt/application'
 
 export const formatWalletAddressForDisplay = function (address: AccountAddressT): string {
   const s = address.toString()
@@ -8,4 +8,46 @@ export const formatWalletAddressForDisplay = function (address: AccountAddressT)
 export const formatValidatorAddressForDisplay = function (address: ValidatorAddressT): string {
   const s = address.toString()
   return s.substring(0, 2) + '...' + s.substring(s.length - 9)
+}
+
+export const truncateRRIStringForDisplay = function (rriString: string): string {
+  // Sort by largest -> Smallest since some suffixes have length 4 and some have 3
+  // Avoiding scenarios such as matching '_tr' first when there is a '_tr3'
+  // If no RRI suffix match, will return regular string
+  // Return original RRI String if <= 21, following Wireframe design examples
+  if (rriString.length <= 21) {
+    return rriString
+  }
+  const sortedHRPKeys = getSortedHRPKeys()
+  const correctRRISuffix = getCorrectRRISuffixFromHRP(sortedHRPKeys, rriString)
+  // Catch in case no RRI Suffix was found in Networks/HRP
+  if (correctRRISuffix === '') {
+    return rriString
+  }
+  const prefixLength = getRRIPrefixLength(correctRRISuffix, rriString)
+  return rriString.substring(0, prefixLength) + '...' + rriString.substring(rriString.length - 9)
+}
+
+const getSortedHRPKeys = function (): string[] {
+  const allHRPKeys = []
+  for (const [key, val] of Object.entries(HRP)) {
+    allHRPKeys.push(val.RRI_suffix)
+  }
+  allHRPKeys.sort(function (a, b) {
+    return b.length - a.length
+  })
+  return allHRPKeys
+}
+
+const getCorrectRRISuffixFromHRP = function (hrpKeys: string[], rriString: string): string {
+  // Using regex /${suffix}/.exec(rriString) to return the right suffix
+  const rriStringMatchHRPKey = hrpKeys.filter((suffix) => {
+    return rriString.match(suffix)! != null
+  })
+  return rriStringMatchHRPKey[0]
+}
+
+const getRRIPrefixLength = function (rriSuffix: string, rriString: string): number {
+  const match = rriString.match(rriSuffix)!
+  return match.index! + rriSuffix.length
 }
--- src/views/Wallet/WalletOverview.vue
+++ src/views/Wallet/WalletOverview.vue
@@ -46,12 +46,35 @@
         <div
           v-for="(tokenBalance, i) in otherTokenBalances"
           :key="i"
-          class="border border-rGray rounded-md divide-x divide-rGray"
+          class="border border-rGray rounded-md  divide-rGray"
         >
+        <div class="flex flex-row py-1">
+          <div class="flex-1 flex flex-row items-center px-6 pt-3 overflow-x-auto">
+            <span class="text-sm text-rGrayDark">{{ tokenBalance.token.name }}</span>
+          </div>
+          <div>
+          <a :href="createOtherTokenUrl(tokenBalance.token.rri.toString())" target="_blank" class="hover:text-rGreen transition-colors text-rGrayMed">
+            <div class="rounded-full border border-solid border-rGray w-6 h-6 flex items-center justify-center mr-1">
+              <svg width="8" height="8" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1.08789 1H11.1344V11.0465" class="stroke-current" stroke-miterlimit="10"/>
+                <path d="M11.1339 1L1 11.134" class="stroke-current" stroke-miterlimit="10"/>
+              </svg>
+            </div>
+          </a>
+          </div>
+        </div>
+        <div class="flex flex-row">
+          <div class="flex-1 flex flex-row items-center px-6 pt-3 overflow-x-auto">
+            <span class="text-sm text-rGrayDark">{{ truncateOtherToken(tokenBalance.token.rri.toString()) }}</span>
+            <div class="hover:text-rGreen flex flex-row items-center cursor-pointer transition-colors">
+              <click-to-copy :address="tokenBalance.token.rri.toString()"/>
+            </div>
+          </div>
+        </div>
           <div class="flex flex-row py-1">
-            <div class="flex-1 flex flex-row items-center px-6 py-7 overflow-x-auto">
-              <big-amount :amount="tokenBalance.amount" class="text-2xl font-light mr-4 text-rBlack" />
-              <token-symbol>{{ tokenBalance.token.symbol.toUpperCase() }}</token-symbol>
+            <div class="flex-1 flex flex-row items-center px-6 py-3 overflow-x-auto">
+              <big-amount :amount="tokenBalance.amount" class="text-5xl font-light mr-4 text-rBlack" />
+              <token-symbol class="mt-3">{{ tokenBalance.token.symbol.toUpperCase() }}</token-symbol>
             </div>
           </div>
         </div>
@@ -68,12 +91,21 @@ import TokenSymbol from '@/components/TokenSymbol.vue'
 import ClickToCopy from '@/components/ClickToCopy.vue'
 import { ref } from '@nopr3d/vue-next-rx'
 import { sumAmounts, subtract, add } from '@/helpers/arithmetic'
+import { truncateRRIStringForDisplay } from '@/helpers/formatter'
+import { createRRIUrl } from '@/helpers/explorerLinks'
 
 const WalletOverview = defineComponent({
   components: {
     BigAmount,
     TokenSymbol,
     ClickToCopy
+  },
+
+  setup () {
+    return {
+      createOtherTokenUrl: createRRIUrl,
+      truncateOtherToken: truncateRRIStringForDisplay
+    }
   },
 
   props: {
