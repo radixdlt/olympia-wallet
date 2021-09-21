cf28d9d95c11d69c9f54f89eca8e065feb713012
Caleb Oller <caleboller@gmail.com>
2021-09-21 15:01:25 -0400

    chore: update styling of non-xrd balances

--- src/components/OtherTokenBalanceListItem.vue
+++ src/components/OtherTokenBalanceListItem.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="border border-rGray rounded-md  divide-rGray">
+  <div class="border border-rGray rounded-md divide-rGray">
     <div class="flex flex-row py-1">
       <div class="flex-1 flex flex-row items-center px-6 pt-3 overflow-x-auto justify-between">
-        <span class="text-sm text-rGrayDark">{{ tokenBalance.token.name }}</span>
+        <span class="text-md text-rGrayDark">{{ tokenBalance.token.name }}</span>
         <div>
           <a :href="rriUrl" target="_blank" class="hover:text-rGreen transition-colors text-rGrayMed">
             <div class="rounded-full border border-solid border-rGray w-6 h-6 flex items-center justify-center ">
@@ -17,17 +17,17 @@
     </div>
     <div class="overflow-x-scroll">
       <div class="flex flex-row absolute">
-        <div class="flex-1 flex flex-row items-center px-6 pt-3 overflow-x-auto">
+        <div class="flex-1 flex flex-row items-center px-6 overflow-x-auto">
           <span class="text-sm font-mono text-rGrayDark">{{ truncateRRIStringForDisplay(tokenBalance.token.rri.toString()) }}</span>
           <div class="hover:text-rGreen flex flex-row items-center cursor-pointer transition-colors">
             <click-to-copy :address="tokenBalance.token.rri.toString()"/>
           </div>
         </div>
       </div>
-      <div class="flex flex-row py-1 mt-8">
+      <div class="flex flex-row py-1 mt-6">
         <div class="flex-1 flex flex-row items-center px-6 py-3">
-          <big-amount :amount="tokenBalance.amount" class="text-5xl font-light mr-4 text-rBlack" />
-          <token-symbol class="mt-3">{{ tokenBalance.token.symbol.toUpperCase() }}</token-symbol>
+          <big-amount :amount="tokenBalance.amount" class="text-2xl font-light mr-4 text-rBlack" />
+          <token-symbol>{{ tokenBalance.token.symbol.toUpperCase() }}</token-symbol>
         </div>
       </div>
     </div>
