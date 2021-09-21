ffc71dd51a971597b1d5077b3f0ad072917089e5
Scott Taylor <staylorwr@users.noreply.github.com>
2021-09-21 15:42:58 -0400

    fix: Change Password and Reveal Seed settings tabs should be disabled without an active network connection

--- src/components/TabsTab.vue
+++ src/components/TabsTab.vue
@@ -1,23 +1,44 @@
 <template>
   <div
     class="w-52 flex items-center justify-center py-5 -mb-px relative z-20 cursor-pointer"
-    :class="{'bg-white border-t border-r border-l border-rGray rounded-t-md': isActive}"
-    @click="$emit('click')"
+    :class="{
+      'bg-white border-t border-r border-l border-rGray rounded-t-md': isActive,
+      'opacity-50 cursor-not-allowed': isDisabled
+    }"
+    @click="handleClick"
   >
     <slot></slot>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, toRef } from 'vue'
 
 const TabsTab = defineComponent({
   props: {
     isActive: {
       type: Boolean,
       required: true
+    },
+    isDisabled: {
+      type: Boolean,
+      required: false,
+      defaultValue: false
     }
-  }
+  },
+
+  setup (props, { emit }) {
+    const isDisabled = toRef(props, 'isDisabled')
+
+    return {
+      handleClick () {
+        if (!isDisabled.value) {
+          emit('click')
+        }
+      }
+    }
+  },
+  emits: ['click']
 })
 
 export default TabsTab
