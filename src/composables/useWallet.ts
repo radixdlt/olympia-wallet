ae743a47edc42f08142efc424bde9d6b9fd4b85e
Scott Taylor <staylorwr@users.noreply.github.com>
2021-09-22 15:24:06 -0400

    fix: Hide token balances after the active account has changed

--- src/composables/useWallet.ts
+++ src/composables/useWallet.ts
@@ -180,7 +180,7 @@ const fetchAccountsForNetwork = (network: Network) => {
         saveDerivedAccountsIndex(0, network)
       }
     })
-  getHardwareWalletAddress(network).then(a => { hardwareAddress.value = a; console.log(a, network) })
+  getHardwareWalletAddress(network).then(a => { hardwareAddress.value = a })
 }
 
 const waitUntilAllLoaded = async () => firstValueFrom(allLoadedObservable)
