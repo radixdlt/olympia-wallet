f9bf4de175c036d51ff090a7e2cff353bea61b4f
Caleb Oller <caleboller@gmail.com>
2021-09-23 18:50:51 -0400

    feat: tweak custom node selection error copy

--- src/views/Settings/SettingsSelectNode.vue
+++ src/views/Settings/SettingsSelectNode.vue
@@ -138,7 +138,7 @@ export default defineComponent({
           .catch(() => {
             // Report errors for non url strings
             setErrors({
-              nodeURL: 'Please enter a valid URL address'
+              nodeURL: 'Please enter a valid URL address for an archive node'
             })
           })
         const networkId = await firstValueFrom(tempRadix.ledger.networkId())
@@ -149,7 +149,7 @@ export default defineComponent({
       } catch (error) {
         // Report errors for urls that don't resolve to a radix network
         setErrors({
-          nodeURL: 'Please enter a valid URL address'
+          nodeURL: 'Please enter a valid URL address for an archive node'
         })
         toast.error('Invalid network, unable to connect')
       }
