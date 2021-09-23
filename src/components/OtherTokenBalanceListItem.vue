3bc74ffc453d9a50da90f1a7802b7654949565c6
Scott Taylor <staylorwr@users.noreply.github.com>
2021-09-23 15:38:50 -0400

    fix: Use RXJS to directly query the API for token balances (#337)
    * fix: Use RXJS to directly query the API for token balances
    
    * fix: Add dashes back
    
    * whoops

--- src/components/OtherTokenBalanceListItem.vue
+++ src/components/OtherTokenBalanceListItem.vue
@@ -2,7 +2,7 @@
   <div class="border border-rGray rounded-md divide-rGray">
     <div class="flex flex-row py-1">
       <div class="flex-1 flex flex-row items-center px-6 pt-3 overflow-x-auto justify-between">
-        <span class="text-md text-rGrayDark">{{ tokenBalance.token.name }}</span>
+        <span class="text-md text-rGrayDark">{{ tokenBalance.tokenIdentifier.name }}</span>
         <div>
           <a :href="rriUrl" target="_blank" class="hover:text-rGreen transition-colors text-rGrayMed">
             <div class="rounded-full border border-solid border-rGray w-6 h-6 flex items-center justify-center ">
@@ -18,16 +18,16 @@
     <div class="overflow-x-scroll">
       <div class="flex flex-row absolute">
         <div class="flex-1 flex flex-row items-center px-6 overflow-x-auto">
-          <span class="text-sm font-mono text-rGrayDark">{{ truncateRRIStringForDisplay(tokenBalance.token.rri.toString()) }}</span>
+          <span class="text-sm font-mono text-rGrayDark">{{ truncateRRIStringForDisplay(tokenBalance.tokenIdentifier.toString()) }}</span>
           <div class="hover:text-rGreen flex flex-row items-center cursor-pointer transition-colors">
-            <click-to-copy :address="tokenBalance.token.rri.toString()"/>
+            <click-to-copy :address="tokenBalance.tokenIdentifier.toString()"/>
           </div>
         </div>
       </div>
       <div class="flex flex-row py-1 mt-6">
         <div class="flex-1 flex flex-row items-center px-6 py-3">
           <big-amount :amount="tokenBalance.amount" class="text-2xl font-light mr-4 text-rBlack" />
-          <token-symbol>{{ tokenBalance.token.symbol.toUpperCase() }}</token-symbol>
+          <token-symbol v-if="token">{{ token.symbol.toUpperCase() }}</token-symbol>
         </div>
       </div>
     </div>
@@ -35,12 +35,15 @@
 </template>
 
 <script lang="ts">
-import { computed, ComputedRef, defineComponent, PropType } from 'vue'
+import { computed, ComputedRef, defineComponent, PropType, ref, toRef, Ref } from 'vue'
 import BigAmount from '@/components/BigAmount.vue'
 import TokenSymbol from '@/components/TokenSymbol.vue'
 import ClickToCopy from '@/components/ClickToCopy.vue'
-import { TokenBalance } from '@radixdlt/application'
+import { SimpleTokenBalance, Token } from '@radixdlt/application'
 import { truncateRRIStringForDisplay } from '@/helpers/formatter'
+import { useRouter } from 'vue-router'
+import { useWallet } from '@/composables'
+import { firstValueFrom } from 'rxjs'
 
 export default defineComponent({
   components: {
@@ -51,7 +54,7 @@ export default defineComponent({
 
   props: {
     tokenBalance: {
-      type: Object as PropType<TokenBalance>,
+      type: Object as PropType<SimpleTokenBalance>,
       required: true
     },
     explorerUrlBase: {
@@ -61,13 +64,23 @@ export default defineComponent({
   },
 
   setup (props) {
+    const token: Ref<Token | null> = ref(null)
+    const tokenBalance = toRef(props, 'tokenBalance')
+    const router = useRouter()
+    const { radix } = useWallet(router)
+
+    firstValueFrom(radix.ledger.tokenInfo(tokenBalance.value.tokenIdentifier)).then((t: Token) => {
+      token.value = t
+    })
+
     const rriUrl: ComputedRef<string> = computed(() =>
-      `${props.explorerUrlBase}/#/tokens/${props.tokenBalance.token.rri.toString()}`
+      `${props.explorerUrlBase}/#/tokens/${props.tokenBalance.tokenIdentifier.toString()}`
     )
 
     return {
       rriUrl,
-      truncateRRIStringForDisplay
+      truncateRRIStringForDisplay,
+      token
     }
   }
 })
