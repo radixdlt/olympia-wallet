d715774d4439c63d974277ec606061359529b0d5
Samuel Yeo <yeosamuel95@gmail.com>
2021-10-21 16:52:52 -0400

    chore: use JS URL constructor to check for invalid url

--- src/helpers/explorerLinks.ts
+++ src/helpers/explorerLinks.ts
@@ -2,13 +2,22 @@ export const createRRIUrl = function (rriString: string): string {
   return `${process.env.VUE_APP_EXPLORER}/#/tokens/${rriString}`
 }
 
-// Currently we're validating Validator URL by seeing if the first substring
-// is https:// or http:// . We may expand on how we validate URL later on.
-// If the Validator URL is NOT exploitable, we return true.
+// Currently we're validating Validator URL by first  having JS URL Constructor catch
+// any errors and then seeing if the first substring is https:// or http://. We may
+// expand on how we validate URL later on. If the Validator URL is NOT exploitable,
+// we return true.
 export const checkValidatorUrlExploitable = function (url: string | null): boolean {
   if (!url) {
     return false
   }
   const urlFormattedToCheck = url.toLowerCase()
+  // If there are issues creating a URL via JS URL Constructor, we will return as false.
+  // This should catch any case not RFC3987 compliant.
+  try {
+    const urlConstructedByJs = new URL(urlFormattedToCheck)
+  } catch {
+    return false
+  }
+
   return urlFormattedToCheck.search('https://') === 0 || urlFormattedToCheck.search('http://') === 0
 }
