2ed0a2425961b0ad661def1d6901bab69c0f9840
Samuel Yeo <yeosamuel95@gmail.com>
2021-10-21 13:28:04 -0400

    chore: create helper fn to validate Validator URL

--- src/helpers/explorerLinks.ts
+++ src/helpers/explorerLinks.ts
@@ -1,3 +1,14 @@
 export const createRRIUrl = function (rriString: string): string {
   return `${process.env.VUE_APP_EXPLORER}/#/tokens/${rriString}`
 }
+
+// Currently we're validating Validator URL by seeing if the first substring
+// is https:// or http:// . We may expand on how we validate URL later on.
+// If the Validator URL is NOT exploitable, we return true.
+export const checkValidatorUrlExploitable = function (url: string | null): boolean {
+  if (!url) {
+    return false
+  }
+  const urlFormattedToCheck = url.toLowerCase()
+  return urlFormattedToCheck.search('https://') === 0 || urlFormattedToCheck.search('http://') === 0
+}
