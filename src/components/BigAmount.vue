56f649823592ebdb637c898e5a361ff597e5d69d
Sam Y <SK-Sam@users.noreply.github.com>
2021-11-22 17:47:44 -0500

    fix: add conditional margin to avoid tooltip from being clipped by parent divs (#381)
    * fix: add conditional margin to avoid tooltip from being clipped by parent divs
    
    * refactor: utilize class bindings and shorten computed function

--- src/components/BigAmount.vue
+++ src/components/BigAmount.vue
@@ -1,7 +1,8 @@
 <template>
   <span class="relative inline-flex flex-col items-center group cursor-pointer" @click.stop="copyText">
     <span>{{numberForDisplay}}</span>
-    <div class="absolute invisible group-hover:visible -mt-full bg-rGrayLightest text-rBlack bottom-full text-xs p-1 rounded-sm shadow border border-solid border-rGrayLight">
+    <div class="absolute invisible group-hover:visible -mt-full bg-rGrayLightest text-rBlack bottom-full text-xs p-1 rounded-sm shadow border border-solid border-rGrayLight"
+    :class="numberLessThanFourDigits && 'ml-32'">
       {{fullNumber}}
     </div>
   </span>
@@ -120,6 +121,10 @@ const BigAmount = defineComponent({
 
     fullNumber (): string {
       return asBigNumber(this.amount, true)
+    },
+
+    numberLessThanFourDigits (): boolean {
+      return asBigNumber(this.amount, false).toString().length < 4
     }
   },
 
