f0fae0fe71f28cb20aec2e7e222587cc5add5cfd
Natasha Johnson <njohnson533@gmail.com>
2022-06-16 11:25:52 -0400

    add SettingsSelectDecimal.vue

---
+++ src/views/Settings/SettingsSelectDecimal.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="p-6">
+        
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
