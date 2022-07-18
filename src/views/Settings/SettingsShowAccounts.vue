6d36c758088ed8656cd87e375924e1398e3ee91d
Natasha Johnson <njohnson533@gmail.com>
2022-07-18 12:37:29 -0400

    added comments for future work

--- src/views/Settings/SettingsShowAccounts.vue
+++ src/views/Settings/SettingsShowAccounts.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="bg-white flex flex-col rounded-md w-full mb-60">
-        settings goes here
+    <div class="bg-white flex flex-col rounded-md w-full h-96">
+        <div class="pt-6 px-6 rounded-md">
+            <p>Hidden Accounts</p>
+            <!-- import hiddenAccounts array, -->
+            <!-- loop through each address and display it to the screen with some way to select it,  -->
+            <!-- once selected update set new hidden accounts array,  -->
+            <!-- then get the updated array to auto update the ui  -->
+        </div>
     </div>
 </template>
 
